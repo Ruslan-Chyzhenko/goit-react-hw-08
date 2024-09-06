beea6261503fbb01064addcf28f1fce53591fc0f
Ruslan-Chyzhenko <144059463+Ruslan-Chyzhenko@users.noreply.github.com>
2024-09-06 15:35:03 +0200

    start_001

--- src/redux/contactsOps.js
+++ src/redux/contactsOps.js
@@ -1,7 +1,7 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-axios.defaults.baseURL = "https://66d445865b34bcb9ab3e2040.mockapi.io";
+axios.defaults.baseURL = "https://connections-api.goit.global/";
 
 export const fetchContacts = createAsyncThunk(
   "contacts/fetchAll",
