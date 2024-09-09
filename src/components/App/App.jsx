15d8944d86c8820b3132061f9b7658e514b7d867
Ruslan-Chyzhenko <144059463+Ruslan-Chyzhenko@users.noreply.github.com>
2024-09-09 17:17:22 +0200

    process_007

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,84 +1,131 @@
 // refreshUser - оновлення користувача за токеном. Базовий тип екшену "auth/refresh".
 // Використовується у компоненті App під час його монтування.
 
-// import { useEffect } from "react";
-// import { useDispatch } from "react-redux";
-// import { fetchContacts } from "../../redux/contactsOps";
-
+import { useEffect, lazy, Suspense } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Route, Routes } from "react-router-dom";
-import HomePage from "../../pages/HomePage/HomePage";
-import LoginPage from "../../pages/LoginPage/LoginPage";
-import RegisterPage from "../../pages/RegisterPage/RegisterPage";
-import ContactsPage from "../../pages/ContactsPage/ContactsPage";
+import clsx from "clsx";
+
+const HomePage = lazy(() => import("../../pages/HomePage/HomePage"));
+const LoginPage = lazy(() => import("../../pages/LoginPage/LoginPage"));
+const RegistrationPage = lazy(() =>
+  import("../../pages/RegistrationPage/RegistrationPage")
+);
+const ContactsPage = lazy(() =>
+  import("../../pages/ContactsPage/ContactsPage")
+);
+
 import Layout from "../Layout";
 import PrivateRoute from "../PrivateRoute";
 import RestrictedRoute from "../RestrictedRoute";
 
-// import ContactList from "../ContactList/ContactList";
-// import ContactForm from "../ContactForm/ContactForm";
-// import SearchBox from "../SearchBox/SearchBox";
-// import css from "../App/App.module.css";
-
-//
+import {
+  selectIsRefreshing,
+  selectIsLoggedIn,
+  selectUser,
+} from "../redux/auth/selectors";
+import { logOut, refreshUser } from "./redux/auth/operations";
 
 export default function App() {
-  // const dispatch = useDispatch();
-  // useEffect(() => {
-  //   dispatch(fetchContacts());
-  // }, [dispatch]);
+  const dispatch = useDispatch();
+  const isRefreshing = useSelector(selectIsRefreshing);
 
-  // return (
-  //   <div className={css.container}>
-  //     <h1>PhoneBook</h1>
-  //     <ContactForm />
-  //     <SearchBox />
-  //     <ContactList />
-  //   </div>
-  // );
-  // }
+  useEffect(() => {
+    dispatch(refreshUser());
+  }, [dispatch]);
+
+  const onLogout = () => {
+    dispatch(logOut());
+  };
+
+  if (isRefreshing) return <p>User is refreshing, please wait</p>;
 
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<HomePage />} />
-        <Route
-          path="/login"
-          element={
-            <RestrictedRoute component={<LoginPage />} redirectTo="/contacts" />
-          }
-        />
-        <Route
-          path="/register"
-          element={
-            <RestrictedRoute
-              component={<RegisterPage />}
-              redirectTo="/contacts"
-            />
-          }
-        />
-        <Route
-          path="/contacts"
-          element={
-            <PrivateRoute component={<ContactsPage />} redirectTo="/login" />
-          }
-        />
-      </Route>
-    </Routes>
+    <div>
+      <header>
+        <nav className={css.nav}>
+          <NavLink
+            class={({ isActive }) => clsx(css.link, isActive && css.active)}
+            to="/"
+          >
+            Home
+          </NavLink>
+          {isLoggedIn ? (
+            <>
+              <NavLink
+                className={({ isActive }) =>
+                  clsx(css.link, isActive && css.active)
+                }
+                to="/contacts"
+              >
+                Contacts
+              </NavLink>
+              <div>
+                <p>Hello, {user.name}!</p>
+                <p>Email: {user.email}</p>
+              </div>
+              <button type="button" onClick={onLogout}>
+                Logout
+              </button>
+            </>
+          ) : (
+            <>
+              <NavLink
+                className={({ isActive }) =>
+                  clsx(css.link, isActive && css.active)
+                }
+                to="/login"
+              >
+                Login
+              </NavLink>
+              <NavLink
+                className={({ isActive }) =>
+                  clsx(css.link, isActive && css.active)
+                }
+                to="/register"
+              >
+                Register
+              </NavLink>
+            </>
+          )}
+        </nav>
+      </header>
+      <main>
+        <Suspense fallback={<Loader />}>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<HomePage />} />
+              <Route
+                path="/login"
+                element={
+                  <RestrictedRoute
+                    component={<LoginPage />}
+                    redirectTo="/contacts"
+                  />
+                }
+              />
+              <Route
+                path="/register"
+                element={
+                  <RestrictedRoute
+                    component={<RegisterPage />}
+                    redirectTo="/contacts"
+                  />
+                }
+              />
+              <Route
+                path="/contacts"
+                element={
+                  <PrivateRoute
+                    component={<ContactsPage />}
+                    redirectTo="/login"
+                  />
+                }
+              />
+            </Route>
+          </Routes>
+        </Suspense>
+      </main>
+    </div>
   );
-}
-
-{
-  /* // return (
-//     <Layout>
-//       <AppBar />
-//       <ContactForm />
-// { */
-}
-{
-  /* //   isLoading && !error && <b>Request in progress...</b>;
-// }
-//       <ContactList />
-//     </Layout>
-//   );
-// }; */
 }
